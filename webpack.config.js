1c83597b124c9ef5b08322753644a44739c0c189
Herz, Andreas <a.herz@freegroup.de>
2018-07-18 14:25:52 +0200

    ...

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 /* global __dirname, require, module*/
 
 const webpack = require('webpack');
-const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 const path = require('path');
 const env = require('yargs').argv.env; // use --env with webpack 2
 const pkg = require('./package.json');
@@ -10,16 +9,14 @@ let libraryName = pkg.name;
 
 let plugins = [], outputFile;
 
-if (env === 'build') {
-  plugins.push(new UglifyJsPlugin({ minimize: true }));
-  outputFile = libraryName + '.min.js';
-} else {
-  outputFile = libraryName + '.js';
-}
+
+outputFile = libraryName + '.js';
+
 
 const config = {
   entry: __dirname + '/src/index.js',
   devtool: 'source-map',
+  mode: 'development',
   output: {
     libraryTarget: 'umd', // make the bundle export
     path: __dirname + '/dist',
