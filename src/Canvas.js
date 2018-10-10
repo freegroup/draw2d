1bef65576a96df5060db8c4effdb0d14e3a2832c
Herz, Andreas <a.herz@freegroup.de>
2018-10-10 16:45:47 +0200

    fix broken demo with scroll container. Accidential wrong width/height because it is overriden by CSS

--- src/Canvas.js
+++ src/Canvas.js
@@ -74,7 +74,7 @@ draw2d.Canvas = Class.extend(
         }
       })
 
-      
+
       // Create the jQuery-Draggable for the palette -> canvas drag&drop interaction
       //
       $(".draw2d_droppable").draggable({
