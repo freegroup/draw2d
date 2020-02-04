c05c4bb8aa38898f042a45db1a710a1ad1584e7d
Herz, Andreas <a.herz@freegroup.de>
2020-02-04 19:29:30 +0100

    no message

--- examples/figure_locator/LabeledEnd.js
+++ examples/figure_locator/LabeledEnd.js
@@ -4,20 +4,7 @@ var LabeledEnd = draw2d.shape.node.End.extend({
     {
       this._super();
     
-      // Create any Draw2D figure as decoration for the connection
-      //
-      this.label = new draw2d.shape.basic.Label({text:"I'm a Label too", color:"#0d0d0d", fontColor:"#0d0d0d"});
-    
-      // add the new decoration to the connection with a position locator.
-      //
-      this.add(this.label, new draw2d.layout.locator.BottomLocator(this));
-    },
-    
-    getLabel:function(){
-    	return this.label.getText();
-    },
-    
-    setLabel: function(text){
-    	this.label.setText(text);
+
+      this.selectionAdapter = null;
     }
 });
