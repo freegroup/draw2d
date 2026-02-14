51be7ba686515bf07008af8a4974f86368fa8f18
draw2d developer <1155039+freegroup@users.noreply.github.com>
2026-02-14 18:54:25 +0100

    docs: update documentation for refactored code
    - Update generated API documentation (data/*.json)
    - Regenerate compiled draw2d.js with modernized code
    - Update example references (box_icon_decoration → box_mixed_layout)
    - Add Markdown shape documentation
    - Remove outdated example files

---
+++ docs/examples/box_mixed_layout/IconDecoShape.js
@@ -0,0 +1,40 @@
+IconDecoShape = draw2d.shape.box.VBox.extend({
+
+	NAME: "IconDecoShape",
+	
+    init : function(attr)
+    {
+        this._super($.extend({bgColor:"#dbddde", color:"#d7d7d7", stroke:1, radius:2, gap:2},attr));
+        
+        
+        this.icons = new draw2d.shape.box.HBox()
+                        .setStroke(0)
+                        .setRadius(this.getRadius())
+                        .setBackgroundColor("#f7f7f7");;
+        
+
+       
+        var img1 = new draw2d.shape.basic.Image({path:"icon.gif", width:30,height:30, resizeable:false});
+        var img2 = new draw2d.shape.basic.Image({path:"icon.gif", width:30,height:30, resizeable:false});
+        var img3 = new draw2d.shape.basic.Image({path:"icon.gif", width:30,height:30, resizeable:false});
+        
+        img1.on("click",function(){alert("hit icon 1");});
+        img2.on("click",function(){alert("hit icon 2");});
+        img3.on("click",function(){alert("hit icon 3");});
+        
+        this.icons.add(img1);                
+        this.icons.add(img2);                
+        this.icons.add(img3);                
+        this.icons.add(new draw2d.shape.basic.Label({text:"icons"}));                
+               
+        // finally compose the shape with top/middle/bottom in VBox
+        //
+        this.add(new draw2d.shape.basic.Label({text:"Header Header Header Header Header"}));
+        this.add(this.icons);
+        this.add(new draw2d.shape.basic.Label({text:"Fixed Footer"}));
+        
+        // resizeable:true => strech the label to the complete width of the shape
+        this.add(new draw2d.shape.basic.Label({text:"Resizeable Footer", resizeable:true}));
+    }
+     
+});
